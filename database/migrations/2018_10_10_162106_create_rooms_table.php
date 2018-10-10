a487e1fe061ce54867fd82b7230a3ea143163a19
Jorge Valenzuela <jivalenzuelaibarra@gmail.com>
2018-10-10 16:01:03 -0300

    creacion de las migraciones

--- database/migrations/2018_10_10_162106_create_rooms_table.php
+++ database/migrations/2018_10_10_162106_create_rooms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoomsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->enum('area', array('area1', 'area2'));
+            $table->string('name');
+            $table->enum('campus', array('Republica', 'Alameda'));
+            $table->string('location');
         });
     }
 
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 }
